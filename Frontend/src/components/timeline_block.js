6d608f2f01187382125a5d739525ff6ad3fcba5c
Aritra Bhaduri <92646038+CodingwithMe123@users.noreply.github.com>
2021-12-10 15:28:55 +0530

    Added and integrated login/signup

--- Frontend/src/components/timeline_block.js
+++ Frontend/src/components/timeline_block.js
@@ -8,7 +8,7 @@ export default function timeline_block(props) {
             <div className="timeline-content">
                 <span className="timeline-year">{props.date}</span>
                 <div className="timeline-icon">
-                <i className="fa fa-apple" aria-hidden="true"></i>
+                {/* <i className="fa fa-apple" aria-hidden="true"></i> causes misbehaviour with fontawesome icons. No visual impact on page. */}
                 </div>
                 <div className="content">
                 {/* <h3 className="title">{props.title}</h3> */}
