79edd0264a36f07077cf325c8ec883555e198e09
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2021-12-10 21:10:06 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -16,6 +16,11 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/SignupStud';
 import SignupMentor from './pages/Signup/SignupMentor';
 import ScrollToTop from './components/ScrollToTop';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Link
+} from 'react-router-dom';
 //import TeamTemp from './pages/leaderboard_temp'
 function App() {
   return (
@@ -24,14 +29,21 @@ function App() {
         <ScrollToTop/>
         <Switch>
           <Route exact path="/" component={Home}/>
-          <Route path="/register" component={Conduct}/>
+          {/* <Route path="/register" component={Conduct}/> */}
           <Route path="/leaderboard" component={Leaderboard} />
           <Route path="/project" component={Project} />
           <Route path="/team" component={Team} />
           <Route path="/reward" component={Reward} />
           <Route path="/login" component={Login} />
-          <Route path="/signup/student" component={Signup} />
-          <Route path="/signup/mentor" component={SignupMentor} />
+          {/* <Route path="/signup/student" component={Signup} />
+          <Route path="/signup/mentor" component={SignupMentor} /> */}
+        </Switch>
+
+
+        <Switch>
+        <Route exact path="/register" component={Conduct}/>
+        <Route path="/register/signup/student" component={Signup}/>
+        <Route path="/register/signup/mentor" component={SignupMentor}/>
         </Switch>
         {/* <Footer/> */}
       </BrowserRouter>
