e5ce6145fe2b31c342ab56bee83a578ab8249a6b
Pratham Choudhary <prathamchoudhary4@gmail.com>
2021-12-16 22:08:20 +0530

    made the mentor profile page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,37 +1,34 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 //import Navigation from './components/Navigation';
-import Home from './pages/Home'
-import ScrollButton from './components/ScrollButton'
-import { Route, Switch, BrowserRouter} from 'react-router-dom';
+import Home from "./pages/Home";
+import ScrollButton from "./components/ScrollButton";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 //import Footer from './components/footer'
-import Conduct from './pages/Conduct.js';
+import Conduct from "./pages/Conduct.js";
 // import Leaderboard from './pages/leaderboard/Leaderboard';
-import Leaderboard from './pages/leaderboard_temp';
-import Project from './pages/Project';
-import Team from './pages/teamnew'
-import Reward from './pages/Reward/reward';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/SignupStud';
-import SignupMentor from './pages/Signup/SignupMentor';
-import SignupCa from './pages/Signup/SignupCA';
-import ScrollToTop from './components/ScrollToTop';
-import ProfileParticipant from './pages/profile/profile_participant';
-import EditParticipant from './pages/profile/edit_participant';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Link
-} from 'react-router-dom';
+import Leaderboard from "./pages/leaderboard_temp";
+import Project from "./pages/Project";
+import Team from "./pages/teamnew";
+import Reward from "./pages/Reward/reward";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/SignupStud";
+import SignupMentor from "./pages/Signup/SignupMentor";
+import SignupCa from "./pages/Signup/SignupCA";
+import ScrollToTop from "./components/ScrollToTop";
+import ProfileParticipant from "./pages/profile/profile_participant";
+import ProfileMentor from "./pages/profile/profile_mentor";
+import EditParticipant from "./pages/profile/edit_participant";
+import { BrowserRouter as Router, Routes, Link } from "react-router-dom";
 //import TeamTemp from './pages/leaderboard_temp'
 function App() {
   return (
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <ScrollToTop/>
+        <ScrollToTop />
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
           {/* <Route path="/register" component={Conduct}/> */}
           <Route path="/leaderboard" component={Leaderboard} />
           <Route path="/project" component={Project} />
@@ -42,21 +39,30 @@ function App() {
           <Route path="/signup/mentor" component={SignupMentor} /> */}
         </Switch>
 
+        <Switch>
+          <Route exact path="/register" component={Conduct} />
+          <Route path="/register/signup/student" component={Signup} />
+          <Route path="/register/signup/mentor" component={SignupMentor} />
+          <Route path="/register/signup/ca" component={SignupCa} />
+        </Switch>
 
         <Switch>
-        <Route exact path="/register" component={Conduct}/>
-        <Route path="/register/signup/student" component={Signup}/>
-        <Route path="/register/signup/mentor" component={SignupMentor}/>
-        <Route path="/register/signup/ca" component={SignupCa}/>
-        </Switch>
-        
-        <Switch>
-          <Route exact path="/login" component={Login}/>
-          <Route path="/login/profile/profile_participant" component={ProfileParticipant}/>
-          <Route path="/login/profile/edit_participant" component={EditParticipant}/>
+          <Route exact path="/login" component={Login} />
+          <Route
+            path="/login/profile/profile_participant"
+            component={ProfileParticipant}
+          />
+          <Route
+            path="/login/profile/profile_mentor"
+            component={ProfileMentor}
+          />
+          <Route
+            path="/login/profile/edit_participant"
+            component={EditParticipant}
+          />
         </Switch>
       </BrowserRouter>
-      <ScrollButton/>
+      <ScrollButton />
     </div>
   );
 }
