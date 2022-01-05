0819f7dfeae7d2f07f8f949a366f633a449008a9
shmiitg <shubhamank002@gmail.com>
2022-01-05 16:57:24 +0530

    login api added

--- Backend/Nodejs/app.js
+++ Backend/Nodejs/app.js
@@ -1,24 +1,26 @@
 const express = require('express');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const session = require('express-session');
+const passport = require('passport');
 
 dotenv.config({ path: './config.env' });
 
-var app=express();
-const PORT = process.env.PORT||3100;
+var app = express();
+const PORT = process.env.PORT || 3100;
 
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'))
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(morgan("dev"));
 app.use(session({
     store: MongoStore.create({
@@ -28,11 +30,19 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(session({
-    secret : 'secret',
-    resave : true,
-    saveUninitialized : true
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
 }));
-app.use('/register',require('./register_route'));
-app.listen(PORT,console.log(`listening at ${PORT}`));
+
+app.use(passport.initialize());
+app.use(passport.session());
+// Passport config 
+const passportInit = require('./config/passport');
+passportInit(passport);
+
+app.use('/register', require('./register_route'));
+app.use('/login', require('./routes/login_route'))
+app.listen(PORT, console.log(`listening at ${PORT}`));
